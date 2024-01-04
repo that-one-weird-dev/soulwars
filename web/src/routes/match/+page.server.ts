2455a15324bb0cbe887f2056a274ce7a13619c33
manu <manuelugoccioni@gmail.com>
2024-01-04 22:05:43 +0100

    fix: obtaning session token not working in prod

--- web/src/routes/match/+page.server.ts
+++ web/src/routes/match/+page.server.ts
@@ -1,23 +1,29 @@
 import { db } from "$lib/server/database";
-import { eq } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 import type { PageServerLoad } from "./$types";
-import { decks } from "$lib/server/schema";
+import { decks, sessions } from "$lib/server/schema";
 import { fail } from "@sveltejs/kit";
 
-export const load: PageServerLoad = async ({ locals, cookies }) => {
+export const load: PageServerLoad = async ({ locals }) => {
     const session = await locals.getSession();
     if (!session?.user?.id) {
         throw fail(400);
     }
 
-    const deckList = db.query.decks.findMany({
+    const deckList = await db.query.decks.findMany({
         where: eq(decks.userId, session.user.id),
     });
 
-    const sessionCookieName = process.env.NODE_ENV === "production" ? "__Secure-authjs.session-token" : "authjs.session-token";
+    const sessionRecord = await db.query.sessions.findFirst({
+        columns: {
+            sessionToken: true,
+        },
+        where: eq(sessions.userId, session.user.id),
+        orderBy: [desc(sessions.expires)],
+    });
 
     return {
         decks: deckList,
-        sessionToken: cookies.get(sessionCookieName)!,
+        sessionToken: sessionRecord?.sessionToken,
     };
 };
